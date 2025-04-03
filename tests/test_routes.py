a0640ad9e89882c63acea8fa51e4e23a0ee140bd
Supriya M <supriyamishra8@gmail.com>
2025-04-03 14:50:44 +0530

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -355,3 +355,13 @@ class TestProductRoutes(TestCase):
         # check the data just to be sure
         for product in data:
             self.assertEqual(product["available"], True)
+    
+   def test_get_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
