753f252f35ec5eb3d49d6ae2e2b03f9d5954de70
Supriya M <supriyamishra8@gmail.com>
2025-04-04 14:42:23 +0530

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,6 +12,7 @@ from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
+from service import talisman
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
@@ -25,14 +26,15 @@ BASE_URL = "/accounts"
 ######################################################################
 class TestAccountService(TestCase):
     """Account Service Tests"""
-
-    @classmethod
-    def setUpClass(cls):
+    
+   @classmethod
+   def setUpClass(cls):
         """Run once before all tests"""
         app.config["TESTING"] = True
         app.config["DEBUG"] = False
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
+        talisman.force_https = False
         init_db(app)
 
     @classmethod
@@ -412,3 +414,9 @@ class TestProductRoutes(TestCase):
                   }
         for key, value in headers.items():
                 self.assertEqual(response.headers.get(key), value)
+    def test_cors_security(self):
+        """It should return a CORS header"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check for the CORS header
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
